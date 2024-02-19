03294cbc60f4d7fc7679b7879edc6f7783850f74
alizamanian79 <alizamaniandev@gmail.com>
2024-02-19 12:09:47 +0330

    api default fix

--- client/src/pages/api/hello.ts
+++ client/src/pages/api/hello.ts
@@ -1,13 +1,44 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+const api = process.env.API;
+const requestMethod = 'GET';
 
-type Data = {
-  name: string;
+
+
+type resData = {
+  message?: string;
+  userId?: number;
+  id?: number;
+  title?: string;
+  completed?: boolean;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<resData>,
 ) {
-  res.status(200).json({ name: "John Doe" });
+  if (req.method === requestMethod) {
+    // Check if the api environment variable is set
+    if (!api) {
+      console.log("API environment variable is not set");
+      return res.status(500).json({ message: "API environment variable is not set" });
+    }
+
+    // Process a GET request
+    fetch(`${api}/1`)
+      .then((response) => response.json())
+      .then((response) =>
+        res.status(200).json(response)
+      )
+
+
+
+    //Handler Api erros
+      .catch((error) => {
+        console.log("Error fetching data:", error);
+        res.status(500).json({ message: "Error fetching data" });
+        return
+      });
+  } else {
+    res.status(401).json({ message: "Request method does not matched !" });
+  }
 }
