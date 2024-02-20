0318056e96cfa29cffadd168b24de682ee705322
alizamanian79 <alizamaniandev@gmail.com>
2024-02-20 11:04:48 +0330

    fix bug

--- client/src/components/Home/ProductCard/ProductCard.tsx
+++ client/src/components/Home/ProductCard/ProductCard.tsx
@@ -5,86 +5,61 @@ import kidsCollection from "../../../../public/productCategories/kidsCollection.
 import womanCollection from "../../../../public/productCategories/womanCollection.png";
 import beautyCollection from "../../../../public/productCategories/beautyCollection.png";
 import homeCollection from "../../../../public/productCategories/Home and living collection.png";
-import Link from "next/link";
 
-const imageStyles =
-  "w-97 h-[97%] object-cover rounded-[2px] hover:cursor-pointer hover:shadow-lg transition-all ease-out delay-75";
+const imageStyles = 'w-97 h-[97%] object-cover rounded-[2px] hover:cursor-pointer hover:shadow-lg transition-all ease-out delay-75';
 
 export default function ProductCard() {
-  const handleClick = (e: React.ChangeEvent, value: string) => {
-    e.preventDefault();
-    alert(value);
-  };
+
+  const handleClick = (e:React.ChangeEvent,value:string) =>{
+    e.preventDefault()
+    alert(value)
+  }
 
   return (
     <div className={`w-100 h-auto flex flex-wrap`}>
-      <Link href="/products/man">
-        <div
-          className={
-            "w-2/4 h-auto max-sm:w-[100%] rmv flex justify-center items-center hover:cursor-pointer "
-          }
-        >
+
+      <div onClick={(e)=>handleClick(e,'man')} className={"w-2/4 h-auto max-sm:w-[100%] rmv flex justify-center items-center hover:cursor-pointer "}>
           <Image
             src={manCollection}
             className={imageStyles}
             alt="Man Collection"
           />
-        </div>
-      </Link>
-      <Link href="/products/man">
-        <div
-          className={
-            "w-1/4 h-auto max-sm:w-[50%] bg-rmv flex justify-center items-center hover:cursor-pointer "
-          }
-        >
-          <Image
+      </div>
+
+      <div onClick={(e)=>handleClick(e,'woman')} className={"w-1/4 h-auto max-sm:w-[50%] bg-rmv flex justify-center items-center hover:cursor-pointer "}>
+      <Image
             src={kidsCollection}
             className={imageStyles}
             alt="Kids Collection"
           />
-        </div>
-      </Link>
-      <Link href="/products/man">
-        <div
-          className={
-            "w-1/4 h-auto max-sm:w-[50%] bg-rmv flex justify-center items-center hover:cursor-pointer "
-          }
-        >
-          <Image
+      </div>
+
+      <div onClick={(e)=>handleClick(e,'woman')} className={"w-1/4 h-auto max-sm:w-[50%] bg-rmv flex justify-center items-center hover:cursor-pointer "}>
+      <Image
             src={womanCollection}
             width={250}
             height={250}
             className={imageStyles}
             alt="Woman Collection"
           />
-        </div>
-      </Link>
-      <Link href="/products/man">
-        <div
-          className={
-            "w-2/4 h-auto max-sm:w-[100%] flex justify-center items-center hover:cursor-pointer "
-          }
-        >
-          <Image
+      </div>
+
+      <div onClick={(e)=>handleClick(e,'beauty')} className={"w-2/4 h-auto max-sm:w-[100%] flex justify-center items-center hover:cursor-pointer "}>
+      <Image
             src={beautyCollection}
             className={imageStyles}
             alt="Beauty Collection"
           />
-        </div>
-      </Link>
-      <Link href="/products/man">
-        <div
-          className={
-            "w-2/4 h-auto max-sm:w-full bg-rmv flex justify-center items-center hover:cursor-pointer "
-          }
-        >
-          <Image
+      </div>
+
+      <div onClick={(e)=>handleClick(e,'home')} className={"w-2/4 h-auto max-sm:w-full bg-rmv flex justify-center items-center hover:cursor-pointer "}>
+      <Image
             src={homeCollection}
             className={imageStyles}
             alt="Home Collection"
           />
-        </div>
-      </Link>
+      </div>
+
     </div>
   );
 }
