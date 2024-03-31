88e08a4095e4c6ab3ce47aab79202dc00685cdb7
alizamanian79 <alizamaniandev@gmail.com>
2024-03-31 13:03:12 +0330

    pattern

--- server/routes/products.js
+++ server/routes/products.js
@@ -4,8 +4,29 @@ const router = express.Router();
 
 
 
+
+pDescription
+pSizes JSON,
+pColors JSON,
+pCategori JSON,
+pTotal INT,
+pPrice INT
+
 router.get(`/${routerName}/pattern`,async(req,res)=>{
-  
+  const data ={
+    "pID":"varchar10",
+    "pSellerID":"varchar8",
+    "pTitle":"varchar45",
+    "pDescription":"varcahr450",
+    "pSizes":"json",
+    "pColors":"json",
+    "pCategori":"json",
+    "pTotal":"json",
+    "pPrice":"json"
+
+    
+  }
+  res.status(200).send(data)
 })
 
 
