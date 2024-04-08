57c43d89c4ff9b076b9394155a10baab6b958423
alizamanian79 <alizamaniandev@gmail.com>
2024-04-08 14:22:08 +0330

    fix space

--- client/src/pages/productpage/[pID]/index.tsx
+++ client/src/pages/productpage/[pID]/index.tsx
@@ -1,17 +1,17 @@
-import  PageProductComponent from "@/components/ProductPage/Layout"
-import { GetServerSideProps } from 'next';
+import PageProductComponent from "@/components/ProductPage/Layout";
+import { GetServerSideProps } from "next";
 
 type Props = {
   id: string;
 };
 
 const Page = ({ id }: Props) => {
-  return (
-    <PageProductComponent param={id}/>
-  );
+  return <PageProductComponent param={id} />;
 };
 
-export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context
+) => {
   const { params } = context;
   const id = params?.pID as string;
 
