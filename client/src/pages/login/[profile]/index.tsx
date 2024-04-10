f65e8d38c07c02f181e0a662ff12e1e12d147ebb
alizamanian79 <alizamaniandev@gmail.com>
2024-04-10 11:31:12 +0330

    Folder created

---
+++ client/src/pages/login/[profile]/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ProfileComponent from '@/components/Profile/Layout'
+export default function Profile() {
+  return (
+    <ProfileComponent />
+  )
+}
---
+++ client/src/pages/login/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Login from '@/components/Login/Layout'
+function LoginComponent() {
+  return (
+    <Login />
+  )
+}
+
+export default LoginComponent
