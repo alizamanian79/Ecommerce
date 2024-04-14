fc7d720164e187e0121f46c255ecab9686b1c4e8
alizamanian79 <alizamaniandev@gmail.com>
2024-04-14 13:50:44 +0330

    react icon

--- client/src/components/GlobalComponents/AddressBar/AddressBar.tsx
+++ client/src/components/GlobalComponents/AddressBar/AddressBar.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-import { faDashboard } from "@fortawesome/free-solid-svg-icons";
+import { IoHomeOutline } from "react-icons/io5";
 
 function AddressBar() {
     
   return (
     <div className={`w-100 h-[55px] border-t-2 border-b-2 border-[#cecece] border-indigo-500 flex items-center justify-center`}>
   <ul className={`w-94 h-full flex items-center justify-start bg-rmv`}>
-        <FontAwesomeIcon icons={faDashboard} />
+        <IoHomeOutline />
         
     <li className={`ml-2 md:ml-4 lg:ml-4 font-["yekanBakht"]`}>خانه</li>
     <li className={`ml-2 md:ml-4 lg:ml-4 font-["yekanBakht"]`}>ورود</li>
