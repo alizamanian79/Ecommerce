8491aa13b541f466e9644a8959b5733f8fe438ce
alizamanian79 <alizamaniandev@gmail.com>
2024-04-27 10:42:59 +0330

    color added and card

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,55 +1,51 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors:{
-
-      'red':'red',
+    colors: {
+      red: "red",
 
       //Home
-      'hBack':'#f4f4f4',
+      hBack: "#f4f4f4",
 
-      'rmv':'transparent',
-      'mainColor':'#232629',
-
+      rmv: "transparent",
+      mainColor: "#232629",
 
       //Categories
-      'ctDesColor':'#606060',
-      'brColor':'#4c4c4c94',
-      
+      ctDesColor: "#606060",
+      brColor: "#4c4c4c94",
+
       //Nav Items Color
-      'mBackColor':'#ffffff',
-      'nvColor':'#000000',
-      'nvHoverColor':'#606060',
-
-
+      mBackColor: "#ffffff",
+      nvColor: "#000000",
+      nvHoverColor: "#606060",
 
       //Season
-      'sBack':'#222529'
+      sBack: "#222529",
 
-
+      //New theme
+      color1: "#374151",
+      color2: "#8b5cf6",
+      color3: "#6b7280",
     },
 
     extend: {
-
-
-
-      width: (value:any) => {
-        const widthValues:any={};
+      width: (value: any) => {
+        const widthValues: any = {};
         for (let i = 1; i <= 100; i++) {
           widthValues[i] = `${i}%`;
         }
         return widthValues;
       },
 
-      height: (value:any) => {
-        const heightValues:any={};
+      height: (value: any) => {
+        const heightValues: any = {};
         for (let i = 1; i <= 100; i++) {
           heightValues[i] = `${i}px`;
         }
